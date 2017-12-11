854c31f7ad2ee616797b3037c7536f202c5a622e
francisco <franciscojavier.andgo@gmail.com>
2017-12-11 22:43:56 +0100

    Lesson 12 Seeders

---
+++ database/seeds/ProfessionSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProfessionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('professions')->insert([
+            'title' => 'Desarrolador back-end',
+        ]);
+
+        DB::table('professions')->insert([
+            'title' => 'Desarrolador front-end',
+        ]);
+
+        DB::table('professions')->insert([
+            'title' => 'Diseñador web',
+        ]);
+    }
+}
